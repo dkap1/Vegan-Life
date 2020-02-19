064f6049d351c5cbf3f783aa976cc5b06715f858
dkap1 <45563618+dkap1@users.noreply.github.com>
2020-02-19 20:48:29 +0000

    Initial Commit

---
+++ Vegan Life/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Vegan Life
+//
+//  Created by Danyaal Kapadia on 19/02/2020.
+//  Copyright © 2020 Danyaal. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Vegan Life/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Vegan Life
+//
+//  Created by Danyaal Kapadia on 19/02/2020.
+//  Copyright © 2020 Danyaal. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
